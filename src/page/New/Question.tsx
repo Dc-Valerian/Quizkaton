737636c039743b944d1e0dfeac703800e608c480
Dc Valerian <Dc-Valerian>
2023-11-15 23:22:40 +0100

    added New Update

--- src/page/New/Question.tsx
+++ src/page/New/Question.tsx
@@ -1,22 +1,73 @@
-// components/Question.tsx
-import React from 'react';
-import Option from './Option';
+// Question.tsx
+
+import React from "react";
+import styled from "styled-components";
 
 interface QuestionProps {
   question: string;
   options: string[];
   onSelect: (selectedOption: string) => void;
+  correctAnswer: string;
+  checking: boolean;
+  greenOptions: string[];
 }
 
-const Question: React.FC<QuestionProps> = ({ question, options, onSelect }) => {
+const Question: React.FC<QuestionProps> = ({
+  question,
+  options,
+  onSelect,
+  checking,
+  greenOptions,
+}) => {
+  const getColor = (option: string): string => {
+    if (checking && greenOptions.includes(option)) {
+      return "green";
+    } else if (checking) {
+      return "red";
+    } else {
+      return "white";
+    }
+  };
+
   return (
     <div>
-      <h3>{question}</h3>
-      {options.map((option, index) => (
-        <Option key={index} text={option} onSelect={onSelect} />
-      ))}
+      <p>{question}</p>
+      <OptionsWrapper>
+        {options.map((option, index) => (
+          <Option
+            key={index}
+            onClick={() => onSelect(option)}
+            color={getColor(option)}
+            cursor={checking ? "pointer" : "default"}
+          >
+            {option}
+          </Option>
+        ))}
+      </OptionsWrapper>
     </div>
   );
 };
 
 export default Question;
+
+const OptionsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+interface OptionProps {
+  color: string;
+  cursor: string;
+}
+
+const Option = styled.div<OptionProps>`
+  padding: 10px;
+  margin: 5px;
+  color: orange;
+  cursor: ${(props) => props.cursor};
+  background-color: ${(props) => props.color};
+
+  &:hover {
+    background-color: yellow;
+  }
+`;
--- src/page/New/Quiz.tsx
+++ src/page/New/Quiz.tsx
@@ -54,15 +54,39 @@ const Quiz: React.FC = () => {
   ];
 
   const [currentQuestion, setCurrentQuestion] = useState<number | null>(null);
+  const [userAnswers, setUserAnswers] = useState<string[]>([]);
   const [userAnswer, setUserAnswer] = useState("");
   const [showResult, setShowResult] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const [timer, setTimer] = useState<number | null>(null);
   const [timeRemaining, setTimeRemaining] = useState<number | null>(null);
+  const [checking, setChecking] = useState(false);
+  const [greenOptions, setGreenOptions] = useState<string[]>([]);
 
   const handleOptionSelect = (selectedOption: string) => {
     setUserAnswer(selectedOption);
     setShowResult(true);
+    if (userAnswers.includes(selectedOption)) {
+      Swal.fire({
+        title: "This question has been answered",
+        text: "Please choose another question.",
+        icon: "error",
+        confirmButtonText: "OK",
+      });
+    } else {
+      setUserAnswers([...userAnswers, selectedOption]);
+
+      // Remove the answered question from the quizData array
+      if (currentQuestion !== null) {
+        const updatedQuizData = [...quizData];
+        updatedQuizData.splice(currentQuestion, 1);
+        // Update the local quizData directly
+        // quizData = updatedQuizData; // Uncomment this line if quizData is a let variable
+        // Otherwise, manage it through state or other appropriate state management
+      }
+
+      // Rest of your logic for handling the selected option
+    }
   };
 
   const handleNextQuestion = () => {
@@ -79,14 +103,23 @@ const Quiz: React.FC = () => {
           title: "Time's up!",
           text: "You ran out of time for this question.",
           icon: "warning",
+          showCancelButton: true, // Show Cancel button
           confirmButtonText: "OK",
-        }).then(() => {
-          // Reset the current question index and hide the result
-          setCurrentQuestion(null);
-          setShowResult(false);
-          // Clear the timer and set it again for the next question
-          clearTimeout(timer as number);
-          setTimeRemaining(0);
+          cancelButtonText: "Cancel", // Set Cancel button text
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // User clicked OK, handle accordingly
+            // Reset the current question index and hide the result
+            setCurrentQuestion(null);
+            setShowResult(false);
+            // Clear the timer and set it again for the next question
+            clearTimeout(timer as number);
+            setTimeRemaining(0);
+          } else if (result.dismiss === Swal.DismissReason.cancel) {
+            // User clicked Cancel, handle accordingly
+            // For example, navigate the user back
+            console.log("User clicked Cancel");
+          }
         });
       } else {
         // User answered before time ran out
@@ -94,16 +127,25 @@ const Quiz: React.FC = () => {
           title: isCorrect ? "Correct!" : "Incorrect!",
           text: isCorrect
             ? `You Picked the correct answer: ${userAnswer}`
-            : `You Picked the wrong answer: ${userAnswer}. The correct answer is: ${quizData[currentQuestionIndex].correctAnswer}`,
+            : `You Picked the wrong answer: ${userAnswer}.`,
           icon: isCorrect ? "success" : "error",
+          showCancelButton: true, // Show Cancel button
           confirmButtonText: "Next Question",
-        }).then(() => {
-          // Reset the current question index and hide the result
-          setCurrentQuestion(null);
-          setShowResult(false);
-          // Clear the timer and set it again for the next question
-          clearTimeout(timer as number);
-          setTimeRemaining(0);
+          cancelButtonText: "Cancel", // Set Cancel button text
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // User clicked Next Question, handle accordingly
+            // Reset the current question index and hide the result
+            setCurrentQuestion(null);
+            setShowResult(false);
+            // Clear the timer and set it again for the next question
+            clearTimeout(timer as number);
+            setTimeRemaining(0);
+          } else if (result.dismiss === Swal.DismissReason.cancel) {
+            // User clicked Cancel, handle accordingly
+            // For example, navigate the user back
+            console.log("User clicked Cancel");
+          }
         });
       }
     }
@@ -157,6 +199,21 @@ const Quiz: React.FC = () => {
     }
   };
 
+  const handleCheck = () => {
+    if (!checking && userAnswer) {
+      setChecking(true);
+
+      // Identify one correct and one wrong answer to highlight
+      const correctAnswer = quizData[currentQuestion as number].correctAnswer;
+      const options = quizData[currentQuestion as number].options.filter(
+        (option) => option !== correctAnswer
+      );
+      const wrongAnswer = options[Math.floor(Math.random() * options.length)];
+
+      setGreenOptions([correctAnswer, wrongAnswer]);
+    }
+  };
+
   useEffect(() => {
     return () => {
       // Cleanup function to clear the timer when the component unmounts
@@ -189,18 +246,30 @@ const Quiz: React.FC = () => {
           question={quizData[currentQuestion].question}
           options={quizData[currentQuestion].options}
           onSelect={handleOptionSelect}
+          correctAnswer={quizData[currentQuestion].correctAnswer}
+          checking={checking}
+          greenOptions={greenOptions}
         />
       )}
 
       {showResult && (
         <div>
           <p>Your answer: {userAnswer}</p>
-          <button onClick={handleNextQuestion}>Next Question</button>
+          <div style={{ display: "flex", gap: "10px" }}>
+            <button onClick={handleCheck} disabled={!userAnswer || checking}>
+              Check
+            </button>
+            {/* {checking && ( */}
+            <>
+              <button onClick={handleNextQuestion}>Next Question</button>
+              <button onClick={() => setChecking(false)}>Cancel</button>
+            </>
+            {/* )} */}
+          </div>
+          {checking && currentQuestion === quizData.length && (
+            <p>Quiz completed! You did great!</p>
+          )}
         </div>
-      )}
-
-      {currentQuestion === quizData.length && (
-        <p>Quiz completed! You did great!</p>
       )}
     </div>
   );
